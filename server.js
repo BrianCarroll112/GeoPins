fa303e34dcd3f9cdf4971c3425e229f5011ffe81
Brian Carroll <briancarroll112@gmail.com>
2019-07-02 11:29:31 -0400

    typeDefs

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 
 const server = new ApolloServer({
-  typeDeffs,
+  typeDefs,
   resolvers
 });
 
-server.listen();
+server.listen().then(({ url }) => {
+  console.log(`Server listening on ${url}`)
+});
